f265db14f0c8dd866704ca412bba84ced4d0f4ef
Sofia <sofiaxrk@gmail.com>
2023-02-08 15:23:04 +0100

    centered checkbox

--- my-app/src/Login.jsx
+++ my-app/src/Login.jsx
@@ -1,59 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./styles/Login.module.css";
-import { FormControl, InputAdornment, TextField } from '@mui/material';
+import {
+  Checkbox,
+  FormControl,
+  TextField,
+} from "@mui/material";
+import LoadingButton from "./Material/LoadingButton";
+import LocalizedStrings from "react-localization";
+
+const LoginLang = require("./LoginPageLang.json");
+let lang = new LocalizedStrings(LoginLang[0]);
 
 export default class Login extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      emailError: false,
+      password: "",
+      passwordError: false,
+      name: "",
       rememberMe: false,
-      showPassword: false
+      showPassword: false,
     };
   }
 
-  onSubmit = (e) => {
-
-  };
-
   render() {
-    const { email, emailError, password, rememberMe, showPassword } = this.state;
+    const { email, emailError, password, rememberMe, showPassword } =
+      this.state;
     return (
       <div id="loginContainer" className={styles.container}>
         <div className={styles.login}>
-            <div className={styles.logo}></div>
-            <FormControl onSubmit={this.onSubmit}>
+          <div className={styles.logo}></div>
+          <div className={styles.subtitle}>Back Office</div>
+          <FormControl onSubmit={this.onSubmit}>
             <TextField
-              id='email'
-              name='email' 
-              type='email' 
-              error={emailError}
-              className={styles.loginUser} 
-              variant='outlined'
-              value={email}   
-            />
-            <TextField
-              id='component-outlined'
-              name='password' 
-              type={showPassword ? 'text' : 'password'} 
-              error={emailError}
-              className={styles.loginUser} 
-              variant='outlined'
-              value={email}   
-            />
-          <h3 className={styles.titleh3}>Login Component</h3>
-          <div>
-            <label>Email</label>
-            <input
+              id="email"
+              name="email"
               type="email"
+              error={emailError}
+              className={styles.loginUser}
+              variant="outlined"
               value={email}
-              onChange={(e) => this.setState({ email: e.target.value })}
             />
-
-            <p className={styles.text}>hola</p>
-          </div>
-        </FormControl>
+            <TextField
+              id="component-outlined"
+              name="password"
+              type={showPassword ? "text" : "password"}
+              error={emailError}
+              className={styles.loginPwd}
+              variant="outlined"
+              value={password}
+              //missing onChange with handleChange function
+              //missing onKeyUp with handleKeyUp function
+            />
+            <label>
+              <Checkbox
+                color="primary"
+                name="rememberMe"
+                value={rememberMe}
+                checked={rememberMe}
+                //missing onChange with handleCheckboxChange function
+              />
+              <span>{lang.rememberMe}</span>
+            </label>
+            <LoadingButton
+              className={styles.loginBtn}
+              label={lang.signin}
+              // missing isLoading and onClick functions
+            />
+          </FormControl>
         </div>
       </div>
     );
